3f27bc1d8ea142a54aa97dea6273d7df683ebe28
Hanafe Mira <miraahanafee@gmail.com>
2025-04-29 14:49:31 +0530

    app context

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -28,7 +28,7 @@
 import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { NavLink, useNavigate } from "react-router-dom";
-import { AppContext } from "../context/AppContext";
+import { AppContext } from "../context/AppContext.jsx";
 
 const Header = () => {
   const navigate = useNavigate();
